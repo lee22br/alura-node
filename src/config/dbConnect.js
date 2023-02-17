b7ba893324f575f6e131b6f879fa3dfb911537fb
Leandro Iglezias <37187608+lee22br@users.noreply.github.com>
2023-02-17 17:45:06 -0300

    Delete dbConnect.js

--- src/config/dbConnect.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-mongoose.connect("mongodb+srv://alura:1234@alura.dfkkgnr.mongodb.net/alura-node");
-// mongoose.connect('mongodb+srv://alura:<password>@alura.dfkkgnr.mongodb.net/?retryWrites=true&w=majority');
-
-let db = mongoose.connection;
-
-export default db;
