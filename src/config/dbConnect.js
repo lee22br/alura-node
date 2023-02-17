cbbfd24f4330506eabb0bc61ca8b1f4ac7abe572
Leandro Iglezias <leeiglezias@gmail.com>
2023-02-17 17:13:54 -0300

    CRUD completo

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose"
 
 mongoose.connect("mongodb+srv://alura:1234@alura.dfkkgnr.mongodb.net/alura-node");
 // mongoose.connect('mongodb+srv://alura:<password>@alura.dfkkgnr.mongodb.net/?retryWrites=true&w=majority');
-mongoose.set('strictQuery', true);
 
 let db = mongoose.connection;
 
